7b18486a7e1f954ea956b6912ce4fec00b690a58
luluilm <helloluluilm@gmail.com>
2025-07-11 08:42:27 +0700

    Add files via upload

---
+++ script.js
@@ -0,0 +1,5 @@
+function showPage(id) {
+  document.querySelectorAll('.page').forEach(p => p.classList.remove('active'));
+  document.getElementById(id).classList.add('active');
+  window.scrollTo(0, 0); // Gulir ke atas halaman saat pindah halaman
+}
